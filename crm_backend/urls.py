b7143b2ed07ee3464ff22fc39fbc90316cc4b6d1
jaffermakki <63753792+jaffermakki@users.noreply.github.com>
2026-06-27 10:30:38 +0530

    Fix formatting of urlpatterns in urls.py

--- crm_backend/urls.py
+++ crm_backend/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),  # <-- this serves your frontend
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
